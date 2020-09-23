05e578e4b057088a571ba37c47affb59328de983
Ather Ahmad <atherahmad49@yahoo.com>
2020-09-23 22:37:09 +0200

    cors installed

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-const connectDB     = require("./config/db")
-const path          = require("path")
-require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const connectDB = require("./config/db");
+const path = require("path");
+require("dotenv").config();
+const cors = require("cors");
 
 connectDB();
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname+"/public")))
-app.use("/api/auth",require("./router/auth"))
-app.use("/api/recovery", require("./router/recovery"))
-app.use("/api/dashboard",require("./router/dashboard"))
-app.use("/api/account", require("./router/account"))
-app.use("/api/products", require("./router/products"))
-app.use("/api/buy", require("./router/buy"))
-app.use("/api/contact", require("./router/contact"))
-app.use("/api/messages", require("./router/messages"))
-app.use("/api/messages", require("./router/messages"))
-app.use("/api/admin",require("./router/admin"))
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(cors());
+app.use("/api/auth", require("./router/auth"));
+app.use("/api/recovery", require("./router/recovery"));
+app.use("/api/dashboard", require("./router/dashboard"));
+app.use("/api/account", require("./router/account"));
+app.use("/api/products", require("./router/products"));
+app.use("/api/buy", require("./router/buy"));
+app.use("/api/contact", require("./router/contact"));
+app.use("/api/messages", require("./router/messages"));
+app.use("/api/messages", require("./router/messages"));
+app.use("/api/admin", require("./router/admin"));
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
