27c4f3abd76e5000c5f94193ec098f801a0e2f31
Ather Ahmad <atherahmad49@yahoo.com>
2021-04-08 14:22:33 +0200

    configs updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const connectDB = async () => {
 
     const mongoose = require("mongoose");
     require('dotenv').config();
-    const db = `mongodb+srv://admin:athermilad2020@ctoc.kqiyj.mongodb.net/CtoC?retryWrites=true&w=majority`
+    const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ctoc.kqiyj.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`
     // Athrt
  //    const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-er0mu.gcp.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
 // Milad    const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@my-projects-phh6v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
